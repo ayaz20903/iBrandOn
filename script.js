9a72bc4cb2c83fe9e132d8c4a2cc589c482045c1
ayaz20903 <ayaz17895@gmail.com>
2025-02-21 11:41:25 +0000

    new updates

--- script.js
+++ script.js
@@ -17,30 +17,50 @@
 //   });
 
 function initSwiper(selector, initialDelay) {
-    setTimeout(() => {
-      new Swiper(selector, {
-        effect: "cube",
-        grabCursor: true,
-        loop:true,
-        autoplay: {
-          delay: 3000, // Every cube rotates every 4 seconds
-          disableOnInteraction: false,
-        },
-        cubeEffect: {
-          shadow: true,
-          slideShadows: true,
-          shadowOffset: 20,
-          shadowScale: 0.94,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-        },
-      });
-    }, initialDelay);
-  }
+  setTimeout(() => {
+    new Swiper(selector, {
+      effect: "cube",
+      grabCursor: true,
+      loop: true,
+      autoplay: {
+        delay: 3000, // Every cube rotates every 4 seconds
+        disableOnInteraction: false,
+      },
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  }, initialDelay);
+}
 
-  initSwiper(".mySwiper1", 0);     // Start immediately
-  initSwiper(".mySwiper2", 1000);  // Start after 1 sec
-  initSwiper(".mySwiper3", 2000);  // Start after 2 sec
-  initSwiper(".mySwiper4", 3000);  // Start after 3 sec
-  initSwiper(".mySwiper5", 4000);  // Start after 4 sec
+initSwiper(".mySwiper1", 0);     // Start immediately
+initSwiper(".mySwiper2", 1000);  // Start after 1 sec
+initSwiper(".mySwiper3", 2000);  // Start after 2 sec
+initSwiper(".mySwiper4", 3000);  // Start after 3 sec
+initSwiper(".mySwiper5", 4000);  // Start after 4 sec
+
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+  document.body.style.overflow = '';
+}
+
+window.onload = function() {
+  setTimeout(function() {
+      document.getElementById("modal").classList.remove("hidden");
+      disableScroll();
+  }, 3000);
+};
+
+document.getElementById("closeModal").addEventListener("click", function() {
+  document.getElementById("modal").classList.add("hidden");
+  enableScroll();
+});
